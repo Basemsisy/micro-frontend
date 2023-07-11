b68802e7290d745aa5c31a93d561847d2ef7966a
Basem <basemsisy@gmail.com>
2023-07-12 02:27:21 +0300

    refactor(container): remove env check

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,14 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
-};
-
-if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.getElementById('root');
-
-  if (devRoot) {
-    mount(devRoot);
-  }
-}
+ReactDOM.render(<App />, document.getElementById('root'));
