698605b85aa1804295723977804ad311f689073e
Basem <basemsisy@gmail.com>
2023-07-12 01:10:05 +0300

    feat(container): webpack prod config

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,11 +1,8 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const packageJson = require('../package.json')
+const packageJson = require('../package.json');
 
-
-console.log(packageJson)
 const devConfig = {
   mode: 'development',
   devServer: {
@@ -20,10 +17,7 @@ const devConfig = {
       remotes: {
         marketing: 'marketing@http://localhost:8081/remoteEntry.js',
       },
-      shared: packageJson.dependencies
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
+      shared: packageJson.dependencies,
     }),
   ],
 };
