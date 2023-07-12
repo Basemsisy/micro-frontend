022339ecdb65340ef3617db1b622ba744b8840f0
Basem <basemsisy@gmail.com>
2023-07-12 20:17:46 +0300

    pipeline(container): add invalidation to exclude index.html from cache

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,6 +3,7 @@ import MarketingApp from './components/MarketingApp';
 const App = () => {
   return (
     <div className='App'>
+      <h1>hello world</h1>
       <MarketingApp />
     </div>
   );
