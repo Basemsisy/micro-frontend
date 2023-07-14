31ee9794b3a5dd7450b930429fee6e4065926322
Basem Elsisy <basemsisy@gmail.com>
2023-07-14 18:24:34 +0300

    fix(marketing): generate unique class names & 404 redirects

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
 const App = () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma',
+  });
+
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route path={'/pricing'} component={Pricing} />
