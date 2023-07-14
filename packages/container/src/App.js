918508ae6a4577785850343150af2f5c7d8dcffa
Basem Elsisy <basemsisy@gmail.com>
2023-07-14 18:24:18 +0300

    fix(container): generate unique class names & 404 redirects

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 const App = () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+  });
+
   return (
     <BrowserRouter>
-      <div className="App">
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div className="App">
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
