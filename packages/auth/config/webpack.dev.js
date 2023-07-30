3ce3d2e8cdd47bceb974d6e17fcda326c3521d5d
Basem Elsisy <basemsisy@gmail.com>
2023-07-30 22:56:51 +0300

    feat(/): configure public path

--- packages/auth/config/webpack.dev.js
+++ packages/auth/config/webpack.dev.js
@@ -1,20 +1,23 @@
-const { merge } = require("webpack-merge");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const commonConfig = require("./webpack.common");
-const packageJson = require("../package.json");
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
-  mode: "development",
+  mode: 'development',
   devServer: {
     port: 8082,
     historyApiFallback: true,
   },
+  output: {
+    publicPath: 'http://localhost:8082/',
+  },
   plugins: [
     new ModuleFederationPlugin({
-      name: "auth",
-      filename: "remoteEntry.js",
+      name: 'auth',
+      filename: 'remoteEntry.js',
       exposes: {
-        "./AuthApp": "./src/bootstrap",
+        './AuthApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
     }),
